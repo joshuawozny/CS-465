9dd4ff01885c3d93f8abd2fe607aa4f9828f04c3
Joshua Wozny <joshua.wozny@snhu.edu>
2024-01-20 20:17:34 -0500

    Added json files for news sidebar items and room types.

--- travlr/app_server/controllers/news.js
+++ travlr/app_server/controllers/news.js
@@ -1,6 +1,12 @@
+/* use the built-in Node JS file system component with its
+fs.readFileSync() method to retrieve the JSON data from news.json */
+var fs = require('fs');
+var newsArticles = JSON.parse(fs.readFileSync('./data/news.json',
+'utf8'));
+
 /* GET travel view */
 const news = (req, res) => {
-res.render('news', { title: 'Travlr Getaways', activePage: 'news'  });
+res.render('news', { title: 'Travlr Getaways', newsArticles  });
 };
 module.exports = {
     news
--- travlr/app_server/controllers/rooms.js
+++ travlr/app_server/controllers/rooms.js
@@ -1,6 +1,12 @@
+/* use the built-in Node JS file system component with its
+fs.readFileSync() method to retrieve the JSON data from rooms.json */
+var fs = require('fs');
+var roomTypes = JSON.parse(fs.readFileSync('./data/rooms.json',
+'utf8'));
+
 /* GET travel view */
 const rooms = (req, res) => {
-res.render('rooms', { title: 'Travlr Getaways', activePage: 'rooms'  });
+res.render('rooms', { title: 'Travlr Getaways', roomTypes });
 };
 module.exports = {
     rooms
