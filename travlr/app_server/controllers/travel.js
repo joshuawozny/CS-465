1e25b00fc3e8e42d8281c4b790b0e21f1b3ed527
Joshua Wozny <joshua.wozny@snhu.edu>
2024-01-20 19:20:49 -0500

    Replace static html with data loop in travel.hbs

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,6 +1,13 @@
+/* use the built-in Node JS file system component with its
+fs.readFileSync() method to retrieve the JSON data from trips.json */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json',
+'utf8'));
+
+
 /* GET travel view */
 const travel = (req, res) => {
-res.render('travel', { title: 'Travlr Getaways', activePage: 'travel' });
+res.render('travel', { title: 'Travlr Getaways', trips });
 };
 module.exports = {
     travel
